9203a2df649ae11a3abec26496db824371867ddc
marti <marti@192.168.1.38>
2021-03-06 17:49:03 +0100

    2. Refactorización del paquete ficheros y FactoriaFuenteDatosFicheros.

--- src/main/java/org/iesalandalus/programacion/biblioteca/mvc/modelo/FactoriaFuenteDatos.java
+++ src/main/java/org/iesalandalus/programacion/biblioteca/mvc/modelo/FactoriaFuenteDatos.java
@@ -1,12 +1,12 @@
 package org.iesalandalus.programacion.biblioteca.mvc.modelo;
 
-import org.iesalandalus.programacion.biblioteca.mvc.modelo.negocio.memoria.FactoriaFuenteDatosMemoria;
+import org.iesalandalus.programacion.biblioteca.mvc.modelo.negocio.ficheros.FactoriaFuenteDatosFicheros;
 
 public enum FactoriaFuenteDatos {
 
 	MEMORIA {
 		public IFuenteDatos crear() {
-			return new FactoriaFuenteDatosMemoria();
+			return new FactoriaFuenteDatosFicheros();
 		}
 	};
 	
