7bdb6393ea291385aabc4b2659b5d4cf4134d5a3
marti <marti@192.168.1.38>
2021-03-06 19:33:30 +0100

    4. Implementación de comenzar/terminar en clase Alumnos e interfaz.

--- src/main/java/org/iesalandalus/programacion/biblioteca/mvc/modelo/negocio/ficheros/Alumnos.java
+++ src/main/java/org/iesalandalus/programacion/biblioteca/mvc/modelo/negocio/ficheros/Alumnos.java
@@ -1,5 +1,13 @@
 package org.iesalandalus.programacion.biblioteca.mvc.modelo.negocio.ficheros;
 
+import java.io.EOFException;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
@@ -12,7 +20,7 @@ import org.iesalandalus.programacion.biblioteca.mvc.modelo.negocio.IAlumnos;
 public class Alumnos implements IAlumnos {
 
 	/*********ATRIBUTOS*********/
-	
+	private static final String NOMBRE_FICHERO_ALUMNOS = "datos" + File.separator + "alumnos.dat";
 	private List<Alumno> coleccionAlumnos;
 	
 		
@@ -24,6 +32,49 @@ public class Alumnos implements IAlumnos {
 		coleccionAlumnos = new ArrayList<>();
 	}
 
+	public void comenzar() {
+		leer();
+	}
+	
+	private void leer() {
+		File ficheroAlumnos = new File(NOMBRE_FICHERO_ALUMNOS);
+		try (ObjectInputStream entrada = new ObjectInputStream(new FileInputStream(ficheroAlumnos))) {
+			Alumno alumno = null;
+			do {
+				alumno = (Alumno) entrada.readObject();
+				insertar(alumno);
+			} while (alumno != null);
+		} catch (ClassNotFoundException e) {
+			System.out.println("No se ha podido encontrar la clase a leer.");
+		} catch (FileNotFoundException e) {
+			System.out.println("No se ha podido abrir el fichero de alumnos.");
+		} catch (EOFException e) {
+			System.out.println("Fichero alumnos leido satisfactoriamente.");
+		} catch (IOException e) {
+			System.out.println("Error inesperado de Entrada/Salida");
+		} catch (OperationNotSupportedException e) {
+			System.out.println(e.getMessage());
+		}
+	}
+	
+	public void terminar() {
+		escribir();
+	}
+	
+	private void escribir() {
+		File ficheroAlumnos = new File(NOMBRE_FICHERO_ALUMNOS);
+		try (ObjectOutputStream salida = new ObjectOutputStream(new FileOutputStream(ficheroAlumnos))) {
+			for (Alumno alumno : coleccionAlumnos) {
+				salida.writeObject(alumno);
+				System.out.println("Fichero alumnos escrito satisfactoriamente.");
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println("No se ha podido crear el fichero de alumnos.");
+		} catch (IOException e) {
+			System.out.println("Error inesperado de Entrada/Salida");
+		}
+	}
+	
 	
 	/**
 	 * Método que devuelve una copia profunda de la colección.
